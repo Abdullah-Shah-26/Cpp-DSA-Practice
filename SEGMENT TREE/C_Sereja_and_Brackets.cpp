0f5138178b1a04277b2e83f218d7638bb948bb64
Abdullah Shah <syedmohammedabdullahshah@example.com>
2025-11-30 21:16:57 +0530

    Classical Segment Tree Problems

---
+++ SEGMENT TREE/C_Sereja_and_Brackets.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fastio                 \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+
+struct info
+{
+  int open, close, full;
+  info()
+  {
+    open = 0;
+    close = 0;
+    full = 0;
+  }
+  info(int o, int c, int f)
+  {
+    open = o;
+    close = c;
+    full = f;
+  }
+};
+
+info merge(info left, info right)
+{
+  info ans = info();
+  ans.full = left.full + right.full + min(left.open, right.close);
+  ans.open = left.open + right.open - min(left.open, right.close);
+  ans.close = left.close + right.close - min(left.open, right.close);
+  return ans;
+}
+
+void build(int idx, int low, int high, const string &s, vector<info> &seg)
+{
+  if (low == high)
+  {
+    seg[idx] = info(s[low] == '(', s[low] == ')', 0);
+    return;
+  }
+  int mid = (low + high) >> 1;
+  build(2 * idx + 1, low, mid, s, seg);
+  build(2 * idx + 2, mid + 1, high, s, seg);
+  seg[idx] = merge(seg[2 * idx + 1], seg[2 * idx + 2]);
+}
+info query(int idx, int low, int high, int l, int r, vector<info> &seg)
+{
+  if (r < low || high < l)
+    return info();
+  if (low >= l && high <= r)
+  {
+    return seg[idx];
+  }
+  int mid = (low + high) >> 1;
+  info left = query(2 * idx + 1, low, mid, l, r, seg);
+  info right = query(2 * idx + 2, mid+1,high, l, r, seg);
+  return merge(left, right);
+}
+void solve()
+{
+  string s;
+  cin >> s;
+  int n = s.size();
+  vector<info> seg(4 * n);
+  build(0, 0, n - 1, s, seg);
+  int q;
+  cin >> q;
+  while (q--)
+  {
+    int l, r;
+    cin >> l >> r;
+    l--;
+    r--;
+
+    if (l < 0 || r >= n || l > r) {
+            cout << 0 << "\n"; // Handle invalid range safety break
+            continue;
+        }
+    info ans = query(0, 0, n - 1, l, r, seg);
+    cout << 2 * ans.full << endl;
+  }
+}
+int main()
+{
+  fastio;
+  solve();
+  return 0;
+}
---
+++ SEGMENT TREE/D_Xenia_and_Bit_Operations.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fastio                 \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+
+void build(int i, int l, int h, vector<int>&arr, vector<int>&seg, int orr)
+{
+  if (l == h)
+  {
+    seg[i] = arr[l];
+    return;
+  }
+  int mid = (l + h) >> 1;
+  build(2 * i + 1, l, mid, arr, seg, !orr);
+  build(2 * i + 2, mid + 1, h, arr, seg, !orr);
+  if (orr)
+  {
+    seg[i] = seg[2 * i + 1] | seg[2 * i + 2];
+  }
+  else
+  {
+    seg[i] = seg[2 * i + 1] ^ seg[2 * i + 2];
+  }
+}
+
+void update(int idx, int l, int h, vector<int>&seg, int orr, int i, int val)
+{
+  if (l == h)
+  {
+    seg[idx] = val;
+    return;
+  }
+  int mid = (l + h) >> 1;
+  if (i <= mid)
+    update(2 * idx + 1, l, mid, seg, !orr, i, val);
+  else
+    update(2 * idx + 2, mid + 1, h, seg, !orr, i, val);
+
+  if (orr)
+  {
+    seg[idx] = seg[2 * idx + 1] | seg[2 * idx + 2];
+  }
+  else
+  {
+    seg[idx] = seg[2 * idx + 1] ^ seg[2 * idx + 2];
+  }
+}
+
+void solve()
+{
+  int n,q;
+  cin >> n >> q;
+  int el = 1 << n;
+ vector<int>arr(el), seg(4*el);
+  for (int i = 0; i < el; i++)
+    cin >> arr[i];
+  
+  if (n % 2 == 0)
+    build(0, 0, el - 1, arr, seg, 0);
+  else
+    build(0, 0, el - 1, arr, seg, 1);
+
+  while (q--)
+  {
+    int i, val;
+    cin >> i >> val;
+    i--;
+    if((n&1) == 0)
+    {
+      update(0,0,el-1,seg,0,i,val);
+    }
+    else 
+    {
+      update(0,0,el-1,seg,1,i,val);
+    }
+    cout << seg[0] << endl;
+  }
+}
+
+int main()
+{
+  fastio;
+    solve();
+  return 0;
+}
