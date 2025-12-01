b3e8a649674b2a42a4b12f4ce19114ae3131cf7f
Abdullah Shah <syedmohammedabdullahshah@example.com>
2025-12-02 00:48:37 +0530

    FenWick Tree + Cordinate compression

---
+++ FENWICK TREE/CordinateCompression.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//! Map The Array Values to Lower Range Values
+//* TC = O(NLogN)
+//* SC = O(N)
+
+int main()
+{
+  vector<int> values = {100, 1000, 1000, 10000, 10000, 100000, 1000000};
+
+  cout << "Before" << endl;
+  for (auto &i : values)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  sort(values.begin(), values.end());
+  map<int, int> m; //* Gives Compressed Value
+  map<int, int> back; //* Gives Original Idx for Compressed Value
+  int n = values.size();
+  int idx = 1;
+
+  for (int i = 0; i < n; i++)
+  {
+    if (!m[values[i]])
+    {
+      m[values[i]] = idx;
+      back[idx] = values[i];
+      idx++;
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    values[i] = m[values[i]];
+  }
+  cout << "After " << endl;
+  for (auto &i : values)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+  cout << "Elements in Map - Back" << endl;
+  for (auto &it : back)
+  {
+    cout << it.first << " " << it.second << endl;
+  }
+}
