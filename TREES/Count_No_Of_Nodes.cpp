6cbb0fb0905cb0f6a2dcbd09150d620b898f86f1
Abdullah Shah <syedmohammedabdullahshah@example.com>
2026-01-14 12:56:41 +0530

    Binary Tree Problems

---
+++ TREES/Count_No_Of_Nodes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    int findLeftHt(Node* root)
+    {
+        int ht = 0;
+        while(root)
+        {
+            ht++;
+            root = root->left;
+        }
+        
+        return ht;
+    }
+    int findRightHt(Node* root)
+    {
+        int  ht  = 0;
+        while(root)
+        {
+            ht++;
+            root = root->right;
+        }
+        return ht;
+    }
+    
+    int countNodes(Node* root) {
+    if(!root) return 0;
+    
+    int lh = findLeftHt(root);
+    int rh = findRightHt(root);
+    
+    if(lh == rh)
+    return (1 << lh) -1;
+    
+    return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
