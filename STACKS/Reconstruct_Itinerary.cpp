6fd8c7394cd529371f20e4df1f01d0a1d57e9996
Abdullah Shah <syedmohammedabdullahshah@gmail.com>
2026-09-07 20:51:11 +0530

    Update

---
+++ STACKS/Reconstruct_Itinerary.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  vector<string> findItinerary(vector<vector<string>> &tickets) {
+    
+    // Mutliset to explore edges in Lexicograpical order
+    unordered_map<string, multiset<string>>adj; 
+    vector<string> ans;
+
+    int n = tickets.size();
+
+    for (auto &e : tickets)
+      adj[e[0]].insert(e[1]);
+
+    stack<string> st;
+    st.push("JFK"); // Fixed starting point
+
+    while (!st.empty()) {
+      string u = st.top();
+
+      if (adj[u].size() == 0) {
+        st.pop();
+        ans.push_back(u);
+      } else {
+        auto v = adj[u].begin();
+        st.push(*v);
+        adj[u].erase(v);
+      }
+    }
+
+    reverse(begin(ans), end(ans));
+
+    return ans;
+  }
+};
